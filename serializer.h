930d8d2b4fa164938ef37da862894782a1053783
YegorCrazy <egor2002288@gmail.com>
2026-01-08 22:03:58 +0300

    better

--- serializer.h
+++ serializer.h
@@ -7,42 +7,19 @@
 namespace serializer
 {
 
-// TODO: ДОБАВЛЕНИЕ НОВОГО ТИПА
 template <class T>
-using PtrVariant = std::variant<int T::*, double T::*, std::string T::*>;
-
-template <class T>
-consteval auto allMembers()
+void serialize(std::stringstream& ss, const T& obj)
 {
     constexpr auto ctx = std::meta::access_context::unchecked();
 
-    constexpr size_t N = std::meta::nonstatic_data_members_of(^^T, ctx).size();
-    std::array<std::pair<std::string, PtrVariant<T>>, N> result;
-
-    size_t i = 0;
-    template for (constexpr auto field : std::define_static_array(std::meta::nonstatic_data_members_of(^^T, ctx)))
-    {
-        result[i] = { std::string(identifier_of(field)), std::meta::extract<[: type_of(field) :] T::*>(field)};
-        ++i;
-    }
-
-    return result;
+    template for (constexpr auto field : define_static_array(nonstatic_data_members_of(^^T, ctx)))
+        ss << identifier_of(field) << " " << obj.[:field:] << std::endl;
 }
 
 template <class T>
-void serializeOld(std::stringstream& ss, const T& obj)
+T deserialize(std::string str)
 {
-    for (const auto& [memberName, memberPtrVar] : allMembers<T>())
-    {
-        ss << memberName << " ";
-        std::visit([&](auto&& v) { ss << obj.*v; }, memberPtrVar);
-        ss << std::endl;
-    }
-}
-
-template <class T>
-T deserializeOld(std::string str)
-{
+    constexpr auto ctx = std::meta::access_context::unchecked();
     T result;
 
     for (const auto& line : str | std::ranges::views::split('\n')
@@ -53,21 +30,16 @@ T deserializeOld(std::string str)
             auto delimPos = line.find(' ');
             auto name = line.substr(0, delimPos);
             auto strVal = line.substr(delimPos + 1);
-            for (const auto& [memberName, memberPtrVar] : allMembers<T>())
+            template for (constexpr auto field : define_static_array(nonstatic_data_members_of(^^T, ctx)))
             {
-                if (memberName == name)
-                    std::visit([&](auto&& v)
-                    {
-                        // TODO: ДОБАВЛЕНИЕ НОВОГО ТИПА
-                        using memType = std::decay_t<decltype(v)>;
-                        if constexpr (std::is_same_v<memType, int T::*>)
-                            result.*v = stoi(strVal);
-                        else if constexpr (std::is_same_v<memType, double T::*>)
-                            result.*v = stod(strVal);
-                        else if constexpr (std::is_same_v<memType, std::string T::*>)
-                            result.*v = strVal;
-                    },
-                    memberPtrVar);
+                if (identifier_of(field) == name)
+                {
+                    auto iss = std::stringstream(strVal);
+                    if (iss >> result.[:field:]; !iss) {
+                        std::print(stderr, "Failed to parse field {} into a {}\n", strVal, display_string_of(type_of(field)));
+                        std::exit(EXIT_FAILURE);
+                    }
+                }
             }
         }
     }
